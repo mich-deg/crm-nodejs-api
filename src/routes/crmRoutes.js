0a6a5e04d1736eed8bc578176055a84b2b891447
Micheal <lemichael1215@gmail.com>
2023-02-14 19:10:42 +0300

    added middleware basics

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,14 @@
 const routes = (app) => {
     app.route("/contact")
     
-    .get((req, res)=> res.send("Get request successful"))
+    .get((req, res, next)=> {
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request type: ${req.method}`)
+        next();
+    },
+    (req, res, next) => {
+    res.send("Get request successful")})
 
     .post((req, res)=> res.send("Post request successful"))
 
