0f4e3033568e2bb5ac870837b4cda2a0333c4830
Micheal <lemichael1215@gmail.com>
2023-02-16 14:45:18 +0300

    added JWT setup into index.js

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import jsonwebtoken from "jsonwebtoken";
 
+import routes from "./src/routes/crmRoutes";
 
-import routes from "./src/routes/crmRoutes"
-
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 // mongoose connection
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/CRMdb", {
-    useNewUrlParser: true 
-})
-
+  useNewUrlParser: true,
+});
 
 // body-parser setup
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-routes(app)
+// setup JWT
+app.use((req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    jsonwebtoken.verfiy(
+      req.headers.authorization.split(" ")[1],
+      "Password",
+      (err, decode) => {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
 
-app.get('/', (req, res)=>
-    res.send(`Node and express server is running on PORT ${PORT}`)
-)
+routes(app);
 
-app.listen(PORT, ()=>{
-    console.log( `Your server is running on port ${PORT}` )
-})
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on PORT ${PORT}`)
+);
+
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+});
