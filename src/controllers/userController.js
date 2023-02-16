03576d4acc3c8bc2ff75e479e09d560ec358a903
Micheal <lemichael1215@gmail.com>
2023-02-16 13:30:27 +0300

    added the controller for login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,3 +27,30 @@ export const register = () => {
     }
   });
 };
+
+export const login = (req, res) => {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. No user found." });
+      } else if (!user.comparePassword(req.body.password, user.hashPassword)) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. Wrong password." });
+      } else {
+        return res.json({
+          token: jwt.sign(
+            { email: user.email, username: user.username, _id: user.id },
+            "Password"
+          ),
+        });
+      }
+    }
+  );
+};
