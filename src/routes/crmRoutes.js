ea75217c46611d70160fbcf48d07a2ef3ea049f3
Micheal <lemichael1215@gmail.com>
2023-02-16 14:19:53 +0300

    added userController to routes

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -6,39 +6,51 @@ import {
   deleteContact,
 } from "../controllers/crmController";
 
+import { login, register, loginRequired } from "../controllers/userController";
+
 const routes = (app) => {
   app
     .route("/contact")
 
     // find all contacts
-    .get((req, res, next) => {
-      //middleware
-      console.log(`Request from: ${req.originalUrl}`);
-      console.log(`Request type: ${req.method}`);
-      next();
-    }, getContacts)
+    .get(
+      (req, res, next) => {
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+      },
+      loginRequired,
+      getContacts
+    )
     // (req, res, next) => {
     // res.send("Get request successful")})
 
     //.post((req, res)=> res.send("Post request successful"))
 
     // post a new contact
-    .post(addNewContact);
+    .post(loginRequired, addNewContact);
 
   app
     .route("/contact/:contactId")
 
     // find specific contact
-    .get(getContactwithID)
+    .get(loginRequired, getContactwithID)
 
     //.put((req, res) => res.send("Put request successful"))
 
     // update a contact
-    .put(updateContact)
+    .put(loginRequired, updateContact)
 
     //.delete((req, res) => res.send("Delete request successful"));
     // delete a contact
-    .delete(deleteContact);
+    .delete(loginRequired, deleteContact);
+
+  //route for register
+  app.route("/auth/register").post(register);
+
+  //route for login
+  app.route("/login").post(login);
 };
 
 export default routes;
